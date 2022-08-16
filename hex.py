bfa0fef4564f0d97b4c1975cb8196992f4be1a6f
Praneet Bose <94593299+PraneetBose@users.noreply.github.com>
2022-08-16 19:27:47 +0530

    Create hex.py

---
+++ hex.py
@@ -0,0 +1,23 @@
+# Python program to draw
+# Spiral Helix Pattern
+# using Turtle Programming
+
+# Python program to draw
+# Rainbow Benzene / hexagon
+# using Turtle Programming
+import turtle
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+turtle.speed(80)
+turtle.bgcolor('black')
+for x in range(180):
+	t.pencolor(colors[x%6])
+	t.width(x//100 + 1)
+	t.forward(x)
+	t.left(59)
+for i in range(100):
+    turtle.circle(5 * i)
+    turtle.circle(-5 * i)
+    turtle.left(i)
+
+turtle.exitonclick()
